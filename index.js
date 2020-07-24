3cdcb68f3a2bf3b207ff06880bf94d1f6631fa4f
Zoe Kirsh <zoekirsh@gmail.com>
2020-07-24 00:33:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ function theBeatlesPlay(musicians, instruments) {
   } return theyPlay
 }
 
-const facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and a boy scout", "He hated the sound of his own voice"];
 
 function johnLennonFacts(facts) {
   const excitedFacts = [];
